9d98d3c60be9ebc45f516d60537f1a8acb55cb82
yuzeng <yuzeng@umich.edu>
2022-11-17 23:35:19 -0500

    fix  news

--- recent-news.js
+++ recent-news.js
@@ -1,19 +1,7 @@
 document.write('\
 <ul>\
 <li>\
-September \'22 - Participated and presented at the Serverless Computing roundtable at University of Waterloo. (related papers: <a href="https://arxiv.org/pdf/2206.13444.pdf" target="_blank">resource-centric serverless</a> and <a href="https://ubc-cirrus-lab.github.io/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a>)\
-</li>\
-<li>\
-June \'22 - Prof. Aastha Mehta and I are awarded a <a href="https://science.ubc.ca/research/stair" target="_blank">UBC Science STAIR</a> grant to pursue our interdisciplinary research.\
-</li>\
-<li>\
-May \'22 - Gave an invited talk on Multi-Cloud Serverless Computing at the IBM Global Technology Outlook seminar series on Future of Cloud Computing. (<a href="https://ubc-cirrus-lab.github.io/papers/socc21_multi-cloud_serverless.pdf" target="_blank">related paper</a>)\
-</li>\
-<li>\
-March \'22 - Excited to be selected as a <a href="https://cloud.google.com/edu/researchers/innovators" target="_blank">Google Cloud Research Innovator</a>.\
-</li>\
-<li>\
-February \'22 - It was a pleasure to give <a href="https://wss.ce.sharif.edu/schedule" target="_blank">an invited talk</a> at my alma mater, Sharif University of Technology.\
+August \'22 - Awarded  <a href=\"https://www.qualcomm.com/research/university-relations/innovation-fellowship/2022-north-america\" target=\"_blank\">Qualcomm Innovation Fellowship</a>! (19 winning teams out of 130+ proposals from top engineering schools in North America.)\
 </li>\
 </ul>\
 ');

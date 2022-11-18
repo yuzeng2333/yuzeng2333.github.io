274f65373a9f841c0ffb334bc5fca7020a5afb44
yuzeng <yuzeng@umich.edu>
2022-11-17 22:19:31 -0500

    update news

--- all-news.js
+++ all-news.js
@@ -1,46 +1,6 @@
 document.write('\
 <ul>\
 <li>\
-September \'22 - Participated and presented at the Serverless Computing roundtable at University of Waterloo. (related papers: <a href="https://arxiv.org/pdf/2206.13444.pdf" target="_blank">resource-centric serverless</a> and <a href="https://ubc-cirrus-lab.github.io/papers/socc21_multi-cloud_serverless.pdf" target="_blank">multi-cloud serverless</a>)\
+August \'22 - Awarded Qualcomm Innovation Fellowship! (19 winning teams out of 130+ proposals from top engineering schools in North America.)\
 </li>\
-<li>\
-June \'22 - Prof. Aastha Mehta and I are awarded a <a href="https://science.ubc.ca/research/stair" target="_blank">UBC Science STAIR</a> grant to pursue our interdisciplinary research.\
-</li>\
-<li>\
-May \'22 - Gave an invited talk on Multi-Cloud Serverless Computing at the IBM Global Technology Outlook seminar series on Future of Cloud Computing. (<a href="https://ubc-cirrus-lab.github.io/papers/socc21_multi-cloud_serverless.pdf" target="_blank">related paper</a>)\
-</li>\
-<li>\
-March \'22 - Excited to be selected as a <a href="https://cloud.google.com/edu/researchers/innovators" target="_blank">Google Cloud Research Innovator</a>.\
-</li>\
-<li>\
-February \'22 - It was a pleasure to give <a href="https://wss.ce.sharif.edu/schedule" target="_blank">an invited talk</a> at my alma mater, Sharif University of Technology.\
-</li>\
-<li>\
-November \'21 - Gave an invited talk at the Data Center Networking workshop.\
-</li>\
-<li>\
-August \'21 - Two papers accepted to <a href="https://acmsocc.org/2021/index.html" target="_blank">SoCC \'21</a>.\
-</li>\
-<li>\
-July \'21 - Visited University of Waterloo virtually to give <a href="https://cs.uwaterloo.ca/events/systems-and-networking-insights-from-two-serverless-characterization-studies" target="_blank">an invited talk at the Systems and Networking Seminar</a>.\
-</li>\
-<li>\
-May \'21 - Honored to give a keynote at the <a href="https://www.dagstuhl.de/en/program/calendar/semhp/?semnr=21201">Dagstuhl Seminar on Serverless Computing</a>!\
-</li>\
-<li>\
-April \'21 - Excited for my <a href="https://research.ubc.ca/more-20m-nserc-funding-awarded-new-projects-led-ubc-researchers">accepted Discovery Grant</a> proposal! Thank you NSERC!\
-</li>\
-<li>\
-March \'21 - Thank you <a href="https://www-01.ibm.com/ibm/cas/canada/">IBM CAS Canada</a> for awarding my proposal! [<a href="https://www-01.ibm.com/ibm/cas/canada/projects?projectId=1139" target="_blank">find out more</a>]\
-</li>\
-<li>\
-July \'20 - Humbled to receive the USENIX ATC Community Award!\
-</li>\
-<li>\
-June \'20 - Completed my PhD defense at Princeton Univeristy! Thankful for my amazing advisor, Dave, and all my friends and family! <a href="https://www.flickr.com/photos/133851305@N08/albums/72157714856030187">Here</a> are some photos.\
-</li>\
-<li>\
-May \'20 - Delighted to announce that I will join the Department of Electrical and Computer Engineering at UBC as an Assistant Professor, starting July 2021.\
-</li>\
-</ul>\
 ');
